70627696c82929751590c3bf75721882beda6172
David Justen <davejusten03@yahoo.com>
2017-05-08 09:16:46 -0400

    layout 1 draft

--- app/src/main/java/com/example/android/a2ndchance/MainActivity.java
+++ app/src/main/java/com/example/android/a2ndchance/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.a2ndchance;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView title = (TextView) findViewById(R.id.title);
+        Typeface face=Typeface.createFromAsset(getAssets(),
+                "VarelaRound-Regular.ttf");
+
+        title.setTypeface(face);
     }
 }
