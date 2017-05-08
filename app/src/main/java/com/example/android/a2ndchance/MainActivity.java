1543c0ea3edcc63db77c05b0757e547277f92362
David Justen <davejusten03@yahoo.com>
2017-05-08 10:12:31 -0400

    welcome layout 2

--- app/src/main/java/com/example/android/a2ndchance/MainActivity.java
+++ app/src/main/java/com/example/android/a2ndchance/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.a2ndchance;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -11,12 +12,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.w3);
 
         TextView title = (TextView) findViewById(R.id.title);
         Typeface face=Typeface.createFromAsset(getAssets(),
                 "VarelaRound-Regular.ttf");
-
         title.setTypeface(face);
+
+        Button signIn = (Button)
     }
 }
