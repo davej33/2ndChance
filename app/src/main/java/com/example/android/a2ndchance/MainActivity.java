9e6b64547c881522705739940097b41f2da7b356
David Justen <davejusten03@yahoo.com>
2017-05-15 08:07:53 -0400

    JobListActivity Added

--- app/src/main/java/com/example/android/a2ndchance/MainActivity.java
+++ app/src/main/java/com/example/android/a2ndchance/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.a2ndchance;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,20 +14,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.w3);
+        setContentView(R.layout.activity_main);
 
         TextView title = (TextView) findViewById(R.id.title);
-        Typeface face=Typeface.createFromAsset(getAssets(),
+        Typeface face =Typeface.createFromAsset(getAssets(),
                 "VarelaRound-Regular.ttf");
         title.setTypeface(face);
 
         Button search = (Button) findViewById(R.id.searchButton);
         search.setTypeface(face);
 
-        Button signIn = (Button) findViewById(R.id.signIn);
-        signIn.setTypeface(face);
+        TextView signInText = (TextView) findViewById(R.id.signInText);
+        signInText.setTypeface(face);
+        TextView signUpText = (TextView) findViewById(R.id.signUpText);
+        signUpText.setTypeface(face);
 
-        Button signUp = (Button) findViewById(R.id.signUp);
-        signUp.setTypeface(face);
+
+    }
+
+    public void searchJobs(View view) {
+        startActivity(new Intent(this, JobListActivity.class));
     }
 }
